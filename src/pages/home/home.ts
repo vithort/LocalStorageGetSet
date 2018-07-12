4a165774ffb56e59f0633f4fef95a0ed1f0252af
vithort <vithort@gmail.com>
2018-07-12 15:44:48 -0300

    Update
    . importar Storage no home.ts
    . implementar os métodos
    . criar botoes para carregar os métodos no home.html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  inputtext: string;
+  key: string = 'username';
 
+  constructor(
+    public navCtrl: NavController
+    ,private storage: Storage
+  ) {
+
+  }
+
+  saveData() {
+    this.storage.set(this.key, this.inputtext);
+  }
+
+  loadData() {
+    this.storage.get(this.key).then((val) => {
+      console.log('Your username is: ', val);
+    });
   }
 
 }
